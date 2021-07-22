69762a8a8820a229a8b15f640d5ed6f2c985b2bf
Devansh Goswami <devansh51100@gmail.com>
2021-07-22 22:14:26 +0530

    added associated pages

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,12 +1,14 @@
 
 var mongoose = require("mongoose");
+var pagesSchema = require('./pagesModel');
 
 var userSchema = new mongoose.Schema({
     name: String,
     email: String,
     userID: String,
     picture: String,
-    accessToken: String
+    accessToken: String,
+    pages: [pagesSchema]
 });
 
 
--- Routes/authentication.js
+++ Routes/authentication.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../Models/userModel');
+const axios = require('axios');
 
-router.post('/',(req,res)=>{
+router.post('/',async(req,res)=>{
     User.findOne({userID: req.body.userID},(err,user)=>{
         if(user){
             user.accessToken = req.body.accessToken;
@@ -12,7 +13,9 @@ router.post('/',(req,res)=>{
         else{
             const {name,email,picture,userID,accessToken} = req.body;
             const image = picture.data.url;
-            const newUser = new User({name,email,picture: image,userID,accessToken});
+            const pages_response = await axios.get(`https://graph.facebook.com/${userID}/accounts?access_token=${accessToken}`);
+            const pages = pages_response.data;
+            const newUser = new User({name,email,picture:image,userID,accessToken,pages});
             
             newUser.save((err,created_user)=>{
                 if(err){
