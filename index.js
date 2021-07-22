59f8068c4f219574e7fa6b6103ae0c254b1bc682
Devansh Goswami <devansh51100@gmail.com>
2021-07-22 22:36:15 +0530

    trying to resolve cors issue

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors({origin:'*'}));
 
 app.use('/api',router);
 
